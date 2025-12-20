a7da4862a1184102f5e94026192c7e83934747bd
louis88 <35930080+louis-88@users.noreply.github.com>
2025-12-20 01:50:55 +0100

    Clean up vite.config.ts by removing comments
    Removed comments and cleaned up the configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // Repo-Name für GitHub Pages
   base: "/hiveprojects/",
-
   build: {
     outDir: "dist",
     emptyOutDir: true,
-
-    // WICHTIG: pages.html direkt als Entry
-    rollupOptions: {
-      input: "./pages.html",
-    },
   },
 });
