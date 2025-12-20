95d70ee22f3c0a7fc4ac85df83c2ef8adc12ddad
louis88 <35930080+louis-88@users.noreply.github.com>
2025-12-20 01:32:44 +0100

    feat: Configure GitHub Pages deployment
    Set up the CI/CD pipeline to deploy the application to GitHub Pages. This includes updating the Vite configuration to use the correct base path and defining the necessary GitHub Actions workflow.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // IMPORTANT: repo name = hiveprojects
+  base: "/hiveprojects/",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
-  }
+  },
 });
